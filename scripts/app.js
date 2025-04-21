2fcd432be687ad42914fc1720dfb4ed1f9a50f06
Prem200204 <romanprem0123@gmail.com>
2025-04-21 11:35:16 +0530

    navbar with responsive

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,8 @@
-console.log("E-Commerce Website Loaded");
+document.addEventListener("DOMContentLoaded", () => {
+  const checkbox = document.getElementById("check");
+  const navMenu = document.querySelector(".nav-menu");
+
+  checkbox.addEventListener("change", () => {
+    navMenu.classList.toggle("active", checkbox.checked);
+  });
+});
